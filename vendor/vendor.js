09d146f364489cb8eec8e934df8de896a28d19a5
crazywolf132 <crazywolf132@gmail.com>
2016-05-17 12:14:29 +1000

    Vendor

---
+++ vendor/vendor.js
@@ -0,0 +1,14 @@
+/* exported vendor */
+
+/* Smart Mirror
+ * Vendor File Definition
+ *
+ * By Brayden Moon
+ *
+ */
+
+var vendor = {
+	'moment.js' : 'moment/moment-with-locales.js',
+	'weather-icons.css': 'weather-icons/weather-icons.css',
+	'font-awesome.css': 'font-awesome-4.5.0/css/font-awesome.min.css'
+};
