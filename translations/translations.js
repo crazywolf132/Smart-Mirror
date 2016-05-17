9902594531d152a204ab0bdeb09bec123423f5cd
crazywolf132 <crazywolf132@gmail.com>
2016-05-17 12:20:37 +1000

    translations.js

---
+++ translations/translations.js
@@ -0,0 +1,16 @@
+/* Smart Mirror
+ * Translation Definition
+ *
+ * By Brayden Moon
+ *
+ */
+
+var translations = {
+	"en" : "translations/en.json",
+	"nl" : "translations/nl.json",
+	"de" : "translations/de.json",
+	"fr" : "translations/fr.json",
+	"fy" : "translations/fy.json",
+	"es" : "translations/es.json",
+	"it" : "translations/it.json",
+};
